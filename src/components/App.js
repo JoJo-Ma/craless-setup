a4e937f02e839caf799eec37dd71815d56b63cc6
Jonathan Massot <jonathan.massot.j@gmail.com>
2022-07-04 15:29:47 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import '../index.css'
+import style from './App.module.css'
+
+const App = () => {
+        return (
+            <div>
+                <h1 className={style.coco}>Hello world subr!</h1>
+            </div>
+        )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /.css$/i,
+                use: ["style-loader", "css-loader"],
+                exclude: /\.module\.css$/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  "style-loader",
+                  {
+                    loader: "css-loader",
+                    options: {
+                      importLoaders: 1,
+                      modules: true,
+                    },
+                  },
+                ],
+                include: /\.module\.css$/,
+              },
+        ]
+    }
+
+}
